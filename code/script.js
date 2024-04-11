a21f7d213dcfb19ebd4f123a8b489d2daff29ca3
hugozera1 <hugorodrigues1052@gmail.com>
2024-04-11 16:57:42 -0300

    novos assets e implementações no script

--- code/script.js
+++ code/script.js
@@ -7,3 +7,25 @@ function tocarSom() {
         imagem.style.transform = "scale(1)";
       }, 250);
   }
+  function trocarImagem() {
+    const torch = document.getElementById("redstone");
+    if (torch.src.match("redstoneoff")) {
+        torch.src = "/assets/redstonetorch.webp";
+    } else {
+        torch.src = "/assets/redstoneoff.webp";
+    }
+    var audio = document.getElementById("audio2");
+    audio.play();
+}
+function OnOff(){
+    const torch = document.getElementById("redstone");
+    const lamp = document.getElementById("lamp");
+    if (torch.src.match("redstoneoff")){
+        lamp.src = "/assets/lamp.webp";
+
+    } else {
+        lamp.src = "/assets/lampon.webp";
+    }
+    
+    
+}
