7c04754ec61d9ac21131a67f7b1f6f130d03e48e
hugozera1 <hugorodrigues1052@gmail.com>
2024-04-14 10:10:28 -0300

    mudando diretorios

---
+++ script.js
@@ -0,0 +1,50 @@
+function tocarSom() {
+    var audio = document.getElementById("audio");
+    audio.currentTime= 0;
+    audio.play();
+    var imagem = document.getElementById("imagem");
+    imagem.style.transform = "scale(1.2)";
+    setTimeout(function() {
+        imagem.style.transform = "scale(1)";
+      }, 250);
+  }
+  function trocarImagem() {
+    const torch = document.getElementById("redstone");
+    if (torch.src.match("redstoneoff")) {
+        torch.src = "/assets/redstonetorch.webp";
+    } else {
+        torch.src = "/assets/redstoneoff.webp";
+    }
+    var audio = document.getElementById("audio2");
+    audio.play();
+    audio.currentTime= 0.2;
+
+}
+function OnOff(){
+    const torch = document.getElementById("redstone");
+    const lamp = document.getElementById("lamp");
+    if (torch.src.match("redstoneoff")){
+        lamp.src = "/assets/lamp.webp";
+
+    } else {
+        lamp.src = "/assets/lampon.webp";
+    }
+    
+    
+}
+function audioporco(){
+    var audio = document.getElementById("audiopig");
+    audio.play();
+}
+function audiosheep(){
+    const sheep = document.getElementById("shep");
+    sheep.src = "/assets/sheep2.webp";
+
+    var audio = document.getElementById("shearsound");
+    audio.play();
+
+}
+function audiocreeper(){
+    var audio = document.getElementById("crepsound");
+    audio.play();
+}
